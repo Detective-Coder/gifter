6223d5c7acd8f1afc2c92bcb0134dffde54e3eef
Detective-Coder <aaronfrankenfield@gmail.com>
2021-05-27 14:18:38 -0400

    updated getbyid method to include user profile

--- Gifter/Repositories/PostRepository.cs
+++ Gifter/Repositories/PostRepository.cs
@@ -67,9 +67,13 @@ namespace Gifter.Repositories
                 using (var cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                          SELECT Title, Caption, DateCreated, ImageUrl, UserProfileId
-                            FROM Post
-                            WHERE Id = @Id";
+                        SELECT p.Id AS PostId, p.Title, p.Caption, p.DateCreated AS PostDateCreated, 
+                               p.ImageUrl AS PostImageUrl, p.UserProfileId,
+                               up.Name, up.Bio, up.Email, up.DateCreated AS UserProfileDateCreated, 
+                               up.ImageUrl AS UserProfileImageUrl
+                        FROM Post p
+                        LEFT JOIN UserProfile up ON p.UserProfileId = up.id
+                        WHERE p.Id = @Id";
 
                     DbUtils.AddParameter(cmd, "@Id", id);
 
@@ -86,6 +90,14 @@ namespace Gifter.Repositories
                             DateCreated = DbUtils.GetDateTime(reader, "DateCreated"),
                             ImageUrl = DbUtils.GetString(reader, "ImageUrl"),
                             UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+                            UserProfile = new UserProfile()
+                            {
+                                Id = DbUtils.GetInt(reader, "UserProfileId"),
+                                Name = DbUtils.GetString(reader, "Name"),
+                                Email = DbUtils.GetString(reader, "Email"),
+                                DateCreated = DbUtils.GetDateTime(reader, "UserProfileDateCreated"),
+                                ImageUrl = DbUtils.GetString(reader, "UserProfileImageUrl"),
+                            },
                         };
                     }
 
@@ -169,17 +181,17 @@ namespace Gifter.Repositories
                 using (var cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                SELECT p.Id AS PostId, p.Title, p.Caption, p.DateCreated AS PostDateCreated,
-                       p.ImageUrl AS PostImageUrl, p.UserProfileId AS PostUserProfileId,
+                        SELECT p.Id AS PostId, p.Title, p.Caption, p.DateCreated AS PostDateCreated,
+                               p.ImageUrl AS PostImageUrl, p.UserProfileId AS PostUserProfileId,
 
-                       up.Name, up.Bio, up.Email, up.DateCreated AS UserProfileDateCreated,
-                       up.ImageUrl AS UserProfileImageUrl,
+                               up.Name, up.Bio, up.Email, up.DateCreated AS UserProfileDateCreated,
+                               up.ImageUrl AS UserProfileImageUrl,
 
-                       c.Id AS CommentId, c.Message, c.UserProfileId AS CommentUserProfileId
-                  FROM Post p
-                       LEFT JOIN UserProfile up ON p.UserProfileId = up.id
-                       LEFT JOIN Comment c on c.PostId = p.id
-              ORDER BY p.DateCreated";
+                               c.Id AS CommentId, c.Message, c.UserProfileId AS CommentUserProfileId
+                          FROM Post p
+                               LEFT JOIN UserProfile up ON p.UserProfileId = up.id
+                               LEFT JOIN Comment c on c.PostId = p.id
+                        ORDER BY p.DateCreated";
 
                     var reader = cmd.ExecuteReader();
 
