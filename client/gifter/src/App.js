10b98d93107c5756680855cab7c62fda065d841f
Detective-Coder <aaronfrankenfield@gmail.com>
2021-06-03 10:08:19 -0400

    added routes for home and posts/add

--- client/gifter/src/App.js
+++ client/gifter/src/App.js
@@ -3,16 +3,21 @@ import "./App.css";
 import { PostProvider } from "./providers/PostProvider";
 import PostList from "./components/PostList";
 import  PostForm from "./components/PostForm";
-import { PostSearch } from "./components/PostSearch"
+import { PostSearch } from "./components/PostSearch";
+import { BrowserRouter as Router } from "react-router-dom";
+import ApplicationViews from "./components/ApplicationViews"
 
 function App() {
   return (
     <div className="App">
       <PostProvider>
         <PostSearch />
-        <PostForm />
-        <PostList />
       </PostProvider>
+      <Router>
+        <PostProvider>
+          <ApplicationViews />
+        </PostProvider>
+      </Router>
     </div>
   );
 }
