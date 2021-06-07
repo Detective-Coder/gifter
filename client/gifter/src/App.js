17079460ec9b29d05b138cf2243f57130056f4d9
Detective-Coder <aaronfrankenfield@gmail.com>
2021-06-07 09:59:48 -0400

    making adjustments to the client side

--- client/gifter/src/App.js
+++ client/gifter/src/App.js
@@ -1,23 +1,33 @@
 import React from "react";
 import "./App.css";
 import { PostProvider } from "./providers/PostProvider";
-import { PostSearch } from "./components/PostSearch";
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import PostSearch from "./components/SearchForm";
 import { BrowserRouter as Router } from "react-router-dom";
-import ApplicationViews from "./components/ApplicationViews"
+import './App.css';
+import { UserProfileProvider } from "./providers/UserProfileProvider";
 import Header from "./components/Header";
+import ApplicationViews from "./components/ApplicationViews";
+
 
 function App() {
   return (
-    <div className="App">
- 
-      <Router>
-        <PostProvider>
+    <Router>
+      <UserProfileProvider>
+      <PostProvider>
           <Header />
-          <PostSearch />
           <ApplicationViews />
-        </PostProvider>
-      </Router>
-    </div>
+          </PostProvider>
+      </UserProfileProvider>
+    </Router>
+    // <div className="App">
+    //   <PostProvider>
+    //     <PostSearch />
+    //       <PostForm />
+    //     <PostList />
+    //   </PostProvider>
+    // </div>
   );
 }
 
